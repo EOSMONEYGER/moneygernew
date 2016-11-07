392833b479f4d967b58aa02c133b387842e88be6
User-pc\User <wooki0600@naver.com>
2016-11-07 19:18:56 +0900

    no message

---
+++ app/src/main/java/com/example/user/moneyger2/CalOriginActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.moneyger2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by User on 2016-11-06.
+ */
+public class CalOriginActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cal_origin);
+    }
+}
